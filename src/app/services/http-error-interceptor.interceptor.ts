34d94553280448c25d35a07c33b6fce45d669c28
Diego Berges <dbzuera@gmail.com>
2021-08-16 09:56:15 +0200

    Configuracion de Prettier

--- src/app/services/http-error-interceptor.interceptor.ts
+++ src/app/services/http-error-interceptor.interceptor.ts
@@ -1,37 +1,39 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+	HttpRequest,
+	HttpHandler,
+	HttpEvent,
+	HttpInterceptor,
 } from '@angular/common/http';
 import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/internal/operators';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class HttpErrorInterceptor implements HttpInterceptor {
+	constructor() {}
 
-  constructor() {}
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(
-      catchError(error => {
-        let errorMessage = '';
-        if (error instanceof ErrorEvent) {
-          // client-side error
-          errorMessage = `Client-side error: ${error.error.message}`;
-        } else {
-          // backend error
-          errorMessage = `Server-side error: ${error.status} ${error.message}`;
-        }
-        
-        console.log(errorMessage);
-        // aquí podrías agregar código que muestre el error en alguna parte fija de la pantalla.
-        // this.errorService.show(errorMessage);
-        return throwError(errorMessage);
-      })
-    );
-  }
+	intercept(
+		request: HttpRequest<any>,
+		next: HttpHandler
+	): Observable<HttpEvent<any>> {
+		return next.handle(request).pipe(
+			catchError((error) => {
+				let errorMessage = '';
+				if (error instanceof ErrorEvent) {
+					// client-side error
+					errorMessage = `Client-side error: ${error.error.message}`;
+				} else {
+					// backend error
+					errorMessage = `Server-side error: ${error.status} ${error.message}`;
+				}
+				//TODO aquí se abrirá la ventana de gestión de errores
+				console.log(errorMessage);
+				// aquí podrías agregar código que muestre el error en alguna parte fija de la pantalla.
+				// this.errorService.show(errorMessage);
+				return throwError(errorMessage);
+			})
+		);
+	}
 }
