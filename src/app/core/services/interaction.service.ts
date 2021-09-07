f825c3cb2b170754e2084a0e00614cd92bcccacf
Diego Berges <dbzuera@gmail.com>
2021-09-07 09:03:10 +0200

    Añadido Metodo GET en Interaction

--- src/app/core/services/interaction.service.ts
+++ src/app/core/services/interaction.service.ts
@@ -1,27 +1,33 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class InteractionService {
 	constructor(private http: HttpClient) {}
-	// get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
-	// 	return this._http.get(`${environment.api_url}${path}`, { params });
-	// }
+	get<T>(
+		endpoint: string,
+		headers: HttpHeaders = new HttpHeaders(),
+		url: string = environment.api_url
+	): Observable<T> {
+		return this.http.get<T>(`${url}${endpoint}`, { headers: headers });
+	}
 
 	// put(path: string, body: Object = {}): Observable<any> {
 	// 	return this._http.put(`${environment.api_url}${path}`, JSON.stringify(body));
 	// }
 
 	post<T>(
-		url: string,
+		url: string = environment.api_url,
 		body: any = {},
 		headers: HttpHeaders = new HttpHeaders(),
-		params: HttpParams = new HttpParams()
+		params: HttpParams = new HttpParams(),
+		endpoint: string = ''
 	): Observable<T> {
-		return this.http.post<T>(url, body, {
+		return this.http.post<T>(`${url}${endpoint}`, body, {
 			headers: headers,
 			params: params,
 		});
