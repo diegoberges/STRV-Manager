839e2316765a556635aec9fbe96138d11a3c6e84
Diego Berges <dbzuera@gmail.com>
2021-12-05 17:09:59 +0100

    Instalacion de zorro y angular actualizado a ^12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { OauthComponent } from './components/oauth/oauth.component';
 // Interceptors
 import { HttpErrorInterceptor } from './core/interceptors/http-error-interceptor.interceptor';
 import { AthleteComponent } from './components/athlete/athlete.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { es_ES } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(es);
 @NgModule({
 	declarations: [
 		AppComponent,
@@ -16,9 +24,10 @@ import { AthleteComponent } from './components/athlete/athlete.component';
 		OauthComponent,
 		AthleteComponent,
 	],
-	imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+	imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
 	providers: [
 		{ provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
+		{ provide: NZ_I18N, useValue: es_ES },
 	],
 	bootstrap: [AppComponent],
 })
