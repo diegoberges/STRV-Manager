599e2ebd95823d4971ff236a9103486f7d0c0ec3
Diego Berges <dbzuera@gmail.com>
2022-07-11 17:04:50 +0200

    prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	client_id: process.env.NG_APP_CLIENT_ID,
-	url: process.env.NG_APP_URL,
-	client_secret: process.env.NG_APP_CLIENT_SECRET,
+	client_id: secrets.NG_APP_CLIENT_ID,
+	url: secrets.NG_APP_URL,
+	client_secret: secrets.NG_APP_CLIENT_SECRET,
 };
