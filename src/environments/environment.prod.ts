4a0d6940855eb75f017e06f15b1397ffa6063ee2
Diego Berges <dbzuera@gmail.com>
2022-07-12 13:07:29 +0200

    fix prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
 	client_id: process.env.NG_APP_CLIENT_ID,
-	url: 'https://diegoberges.github.io/STRV-Manager/',
+	url: 'https://diegoberges.github.io/STRV-Manager',
 	client_secret: process.env.NG_APP_CLIENT_SECRET,
 };
