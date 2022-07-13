eef4bc91aa5823f7b6aed8433f4693bd6d1fd98a
Diego Berges <dbzuera@gmail.com>
2022-07-13 18:45:07 +0200

    error url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
 	client_id: process.env.NG_APP_CLIENT_ID,
-	url: 'http://strv-manager-diegoberges.vercel.app/',
+	url: 'http://strv-manager-diegoberges.vercel.app',
 	client_secret: process.env.NG_APP_CLIENT_SECRET,
 };
