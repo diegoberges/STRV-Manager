b81c7c127af58d7dbf4dcb1b18826837f8f4dd8c
Diego Berges <dbzuera@gmail.com>
2022-07-13 18:42:16 +0200

    add new domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
 	client_id: process.env.NG_APP_CLIENT_ID,
-	url: 'https://diegoberges.github.io/STRV-Manager',
+	url: 'http://strv-manager-diegoberges.vercel.app/',
 	client_secret: process.env.NG_APP_CLIENT_SECRET,
 };
